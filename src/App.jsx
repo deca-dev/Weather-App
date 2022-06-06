4ec217b42cc2a3e8c2ca1323c433a25f7dfa40b1
David Castro <david.castroanaya@gmail.com>
2022-06-05 23:33:58 -0500

    fixing icons folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import axios from 'axios'
 import 'boxicons'
 
 export const WeatherIcons = {
-  "01d": '/src/assets/icons/sunny.svg',
-  "01n": '/src/assets/icons/night.svg',
-  "02d": '/src/assets/icons/day.svg',
-  "02n": '/src/assets/icons/cloudy-night.svg',
-  "03d": '/src/assets/icons/cloudy.svg',
-  "03n": '/src/assets/icons/cloudy.svg',
-  "04d": '/src/assets/icons/perfect-day.svg',
-  "04n": '/src/assets/icons/cloudy-night.svg',
-  "09d": '/src/assets/icons/rain.svg',
-  "09n": '/src/assets/icons/rain-night.svg',
-  "10d": '/src/assets/icons/rain.svg',
-  "10n": '/src/assets/icons/rain-night.svg',
-  "11d": '/src/assets/icons/storm.svg',
-  "11n": '/src/assets/icons/storm.svg',
-  "13d": '/src/assets/icons/snow-day.svg',
-  "13n": '/src/assets/icons/snow-night.svg',
-  "50d": '/src/assets/icons/mist-day.svg',
-  "50n": '/src/assets/icons/mist-night.svg',
+  "01d": './src/assets/icons/sunny.svg',
+  "01n": './src/assets/icons/night.svg',
+  "02d": './src/assets/icons/day.svg',
+  "02n": './src/assets/icons/cloudy-night.svg',
+  "03d": './src/assets/icons/cloudy.svg',
+  "03n": './src/assets/icons/cloudy.svg',
+  "04d": './src/assets/icons/perfect-day.svg',
+  "04n": './src/assets/icons/cloudy-night.svg',
+  "09d": './src/assets/icons/rain.svg',
+  "09n": './src/assets/icons/rain-night.svg',
+  "10d": './src/assets/icons/rain.svg',
+  "10n": './src/assets/icons/rain-night.svg',
+  "11d": './src/assets/icons/storm.svg',
+  "11n": './src/assets/icons/storm.svg',
+  "13d": './src/assets/icons/snow-day.svg',
+  "13n": './src/assets/icons/snow-night.svg',
+  "50d": './src/assets/icons/mist-day.svg',
+  "50n": './src/assets/icons/mist-night.svg'
 }
 
 
